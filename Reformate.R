410eb34e4d5ffa58d3d3aa48360c45a5b24fdc47
Hou Ze Yu <laha610311101@gmail.com>
2017-09-28 03:24:23 +0800

    A

--- Reformate.R
+++ Reformate.R
@@ -2,44 +2,40 @@
 library("slam");
 library("tm");
 library("dplyr");
+library("data.table");
 ##### --------------
 rm(list = ls());
-train <- fread("train.csv");
-train <-
-  train %>%
-  # Remove missing
-  na.omit %>%
-  mutate(
-    Survived = as.factor(Survived),
-    Pclass = as.factor(Pclass),
-    Sex = as.factor(Sex),
-    Embarked = as.factor(Embarked),
-    Age = as.numeric(Age),
-    SibSp = as.numeric(SibSp),
-    Parch = as.numeric(Parch),
-    Fare = as.numeric(Fare),
-    Ticket = substr(Ticket, 1, 1) %>% as.factor()
-  ) %>%
-  as.data.table;
-# Reformate Name
-reformateName <- train %>% 
-  select(Name) %>%
-  DataframeSource %>%
-  Corpus %>%
-  tm_map(content_transformer(removeNumbers)) %>%
-  tm_map(content_transformer(tolower)) %>%
-  tm_map(content_transformer(removePunctuation)) %>%
-  TermDocumentMatrix(control=list(wordLengths = c(1, Inf)))
-# High words freq term
-row_sums(reformateName) %>% sort %>% tail(20);
-# Finally
-train <- cbind(
-  train,
-  reformateName[
-    c("mr","miss","mrs","william","master","john","henry"),
-    ] %>% as.matrix() %>% t %>% as.data.table
+Full <- merge(read.csv("train.csv"),read.csv("test.csv"), all = T); data.table(Full);
+# PassengerId V
+# Pclass V
+Full <- mutate(Full, Pclass = as.factor(Pclass));
+# Name V
+Full <- Full %>% mutate(
+  Name = if_else(grepl("Mr", Name), "Mr",
+                 if_else(grepl("Mrs", Name), "Mrs",
+                         if_else(grepl("Miss", Name), "Miss","other")))
 );
-# Clear other
-rm(reformateName)
+Full <- mutate(Full,Name = as.factor(Name))
+# Sex V
+# Family V
+Full <- Full %>% mutate(family = SibSp + Parch);
+# SibSp V
+# Parch V
+# Ticket V
+Full <- Full %>% mutate(Ticket = as.factor(substr(Full$Ticket,1,1)));
+# Fare V
+# Cabin V
+Full <- Full %>% mutate(Cabin = gsub("[0-9 ]","",Cabin));
+Full <- Full %>% mutate(Cabin = if_else(nchar(Cabin)==1,Cabin,
+                                        if_else(nchar(Cabin)>1,"more","unknown")));
+Full <- Full %>% mutate(Cabin = as.factor(Cabin));
+# Embarked V
+Full$Embarked <- as.character(Full$Embarked);
+Full$Embarked[Full$Embarked==""] <- NA;
+Full$Embarked <- as.factor(Full$Embarked);
+# Survived V
+Full <- Full %>% mutate(Survived = as.factor(Survived))
+data.table(Full)
+
 
 
